29c140a3ea3a0883776a44558436b91cc3a78ad4
Sophia Shaikh <sophias930@gmail.com>
2020-02-22 20:17:04 -0500

    all questions on the site! ugly tho

--- controller.js
+++ controller.js
@@ -1,23 +1,55 @@
-
+let questionCounter = 0;
 
 $(document).ready(() => {
     // loop through all the questions... re-fill the boxes accordingly 
-    loopThroughQuestions();
+    let firstQuestion = questionBank.get(0);
+    let questionContainer = $('.question');
+    questionContainer.text(firstQuestion[0]);
 
+    let formContainer = $('#answers');
+    for (let i = 1; i < firstQuestion.length; i++) {
+        formContainer.append('<input type="checkbox" id="answer'+i+'" name="answer'+i+'" value="'+firstQuestion[i]+'">');
+        formContainer.append('<label for="answer'+i+'">' + firstQuestion[i] + '</label><br>');
+    }
+
+    formContainer.append('<button onclick=submitted() type="button">Submit</button>');
+
+   
 })
 
-
-function loopThroughQuestions() {
-    // TODO will have all questions show up
+function submitted() {
     // TODO will store all the information from each question
+        // get the info 
 
-    $('#questions').submit(function() {
-        // get all the inputs into an array.
-        var inputs = $('#questions :input');
-        // do something w this.. 
+    nextQuestion();
+}
 
-        for (let i = 0; i < inputs.length; i++) {
-            console.log(inputs[i]);
+
+function nextQuestion() {
+    // will have all questions show up
+    questionCounter++;
+    if (questionCounter > 2) {
+        // we done... 
+        questionsDone();
+    } else {
+        let questionContainer = $('.question');
+        let currentQuestion = questionBank.get(questionCounter);
+        questionContainer.text(currentQuestion[0]);
+    
+        let formContainer = $('#answers');
+        formContainer.empty();
+        for (let i = 1; i < currentQuestion.length; i++) {
+            formContainer.append('<input type="checkbox" id="answer'+i+'" name="answer'+i+'" value="'+currentQuestion[i]+'">')
+            formContainer.append('<label for="answer'+i+'">' + currentQuestion[i] + '</label><br>')
         }
-    });
+        formContainer.append('<button onclick=nextQuestion() type="button">Submit</button>');
+    }
+}
+
+function questionsDone() {
+    console.log("You've answered all questions");
+    $('#entirepage').empty();
+    $('#entirepage').append('<h2>Here are recommended grocery plans catered to you!</h2>');
+    $('#entirepage').append('<p>INSERT PLANS HERE</p>');
+
 }
